01b6fa67844526e0feca3e5ccd9b521c5386175a
kamiliaps <76583821+kamiliaps@users.noreply.github.com>
2023-10-16 18:02:07 +0100

    Fix median.py

--- median.py
+++ median.py
@@ -5,22 +5,21 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-
-        numbers.sort()
-        mid = len(numbers)//2
-        
-            if len(numbers)%2==0:
-                median = (numbers[mid]+numbers[mid-1])/2
-    
-            elif len(numbers) == 1:
-                median = numbers[0]
-    
-            else:
-                median = numbers[mid]
-    
-        print(median)
-        
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
+
+numbers.sort()
+mid = len(numbers)//2
+
+if len(numbers)%2==0:
+    median = (numbers[mid]+numbers[mid-1])/2
+
+elif len(numbers) == 1:
+    median = numbers[0]
+else:
+    median = numbers[mid]
+
+print(median)
+
