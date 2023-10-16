a3a38390803f2e7695e1950ef674af9122b7489d
kamiliaps <76583821+kamiliaps@users.noreply.github.com>
2023-10-16 17:59:16 +0100

    Sort numbers list and calculate median

--- median.py
+++ median.py
@@ -5,21 +5,22 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+
+        numbers.sort()
+        mid = len(numbers)//2
+        
+            if len(numbers)%2==0:
+                median = (numbers[mid]+numbers[mid-1])/2
+    
+            elif len(numbers) == 1:
+                median = numbers[0]
+    
+            else:
+                median = numbers[mid]
+    
+        print(median)
+        
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-sorted_numbers = numbers.sort()
-mid = len(sorted_numbers)/2
-
-if len(sorted_numbers)%2==0:
-    median = (sorted_numbers[mid]+sorted_numbers[mid-1])/2
-
-elif len(sorted_numbers) == 1:
-    median = sorted_numbers[0]
-
-else:
-    median = sorted_numbers[mid]
-
-print(median)
-    
